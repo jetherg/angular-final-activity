d594fcbfdcbb3e437ab3dbf1c3f2f0819210762b
JetherG <JetherG@cloudstaff.com>
2020-12-18 01:21:50 +0800

    finished routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { TicketsComponent } from './pages/tickets/tickets.component';
+import { TicketListComponent } from './pages/tickets/ticket-list/ticket-list.component';
+import { TicketViewComponent } from './pages/tickets/ticket-view/ticket-view.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:PagesComponent, children: [
+      { path:'',component:HomeComponent},
+      { path:'login',component:LoginComponent},
+      { path:'profile',component:ProfileComponent},
+      { path:'tickets',component:TicketsComponent ,children:[
+        {path:'',component:TicketListComponent},
+        {path:'tickets/:id/view',component:TicketViewComponent}
+      ]},
+      { path:'about-us',component:AboutUsComponent},
+    ]
+  },
+  {path:'not-found',component:NotFoundComponent},
+  {path:'**', redirectTo:'not-found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TicketsComponent } from './pages/tickets/tickets.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { SideBarComponent } from './pages/side-bar/side-bar.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TicketViewComponent } from './pages/tickets/ticket-view/ticket-view.component';
+import { TicketListComponent } from './pages/tickets/ticket-list/ticket-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    PagesComponent,
+    HomeComponent,
+    TicketsComponent,
+    AboutUsComponent,
+    SideBarComponent,
+    ProfileComponent,
+    LoginComponent,
+    TicketViewComponent,
+    TicketListComponent
   ],
   imports: [
     BrowserModule,
